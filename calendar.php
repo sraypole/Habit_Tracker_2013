ab79e6663cd975e3ee03dba87c4f4e5be802dd3d
cpcarpenter <cpcarpenter52@gmail.com>
2013-12-11 11:43:12 -0500

    Updated calendar
    Still needs connection to database and link to habit page (where it says test.html)

--- calendar.php
+++ calendar.php
@@ -19,28 +19,23 @@ tr.calendar-row {}
 
 td.calendar-day	
 { 
-	min-height:80px; 
+	min-height:70px; 
 	font-size:11px; 
 	position:relative; 
-} * html div.calendar-day { height:80px; }
-
-td.calendar-day:hover	
-{ 
-	background:#ECEFF5; 
-}
+} * html div.calendar-day { height:70px; }
 
 td.calendar-day-blank	
 { 
 	background:#EEE; 
-	min-height:80px; 
-} * html div.calendar-day-blank { height:80px; }
+	min-height:70px; 
+} * html div.calendar-day-blank { height:70px; }
 
 td.calendar-day-head 
 { 
 	background:#CCC; 
 	font-weight:bold; 
 	text-align:center; 
-	width:120px; 
+	width:110px; 
 	padding:5px; 
 	border-bottom:1px solid #000; 
 	border-top:1px solid #000; 
@@ -55,7 +50,7 @@ div.day-number
 	font-size:14px; 
 	font-weight:bold; 
 	float:left; 
-	margin:-34px -5px 0 0; 
+	margin:0px -5px 0 0; 
 	width:20px; 
 	text-align:center; 
 	border-bottom:1px solid #000; 
@@ -65,8 +60,8 @@ div.day-number
 /* Shared style */
 td.calendar-day, td.calendar-day-blank 
 { 
-	width:120px; 
-	height:80px; 
+	width:110px; 
+	height:70px; 
 	padding:0px; 
 	border-bottom:1px solid #000; 
 	border-right:1px solid #000; 
@@ -80,79 +75,110 @@ td.calendar-day, td.calendar-day-blank
 
 $month = array("January","February","March","April","May","June","July","August","September","October","November","December");
 
-function draw_calendar($month, $year)
+echo "<h2>".$month[date('m') - 1]." ".date('Y')."</h2>";
+
+//Draw table
+
+echo '<table cellpadding="0" cellspacing="0" class="calendar">';
+
+//Table headings
+$headings = array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+echo '<tr class="calendar-row"><td class="calendar-day-head">'.implode('</td><td class="calendar-day-head">',$headings).'</td></tr>';
+
+//Variables for days and weeks
+$running_day = date('w',mktime(0,0,0,date('m'),1,date('y')));
+$days_in_month = date('t',mktime(0,0,0,date('m'),1,date('y')));
+$days_in_week = 1;
+$day_counter = 0;
+
+//Row for week one
+
+echo '<tr class="calendar-row">';
+
+//Print "blank" days until first day of the current month
+for ($x = 0; $x < $running_day; $x++)
 {
-	//Draw table
-	$calendar = '<table cellpadding="0" cellspacing="0" class="calendar">';
-
-	//Table headings
-	$headings = array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
-	$calendar.= '<tr class="calendar-row"><td class="calendar-day-head">'.implode('</td><td class="calendar-day-head">',$headings).'</td></tr>';
-
-	//Variables for days and weeks
-	$running_day = date('w',mktime(0,0,0,$month,1,$year));
-	$days_in_month = date('t',mktime(0,0,0,$month,1,$year));
-	$days_in_week = 1;
-	$day_counter = 0;
-
-	//Row for week one
-	$calendar.= '<tr class="calendar-row">';
-
-	//Print "blank" days until first day of the current month
-	for ($x = 0; $x < $running_day; $x++)
-	{
-		$calendar.= '<td class="calendar-day-blank"> </td>';
-		$days_in_week++;
-	}
-
-	//Continue days of the month
-	for ($list_day = 1; $list_day <= $days_in_month; $list_day++)
-	{
-		$calendar.= '<td class="calendar-day">';
-			//Add in number for day
-			$calendar.= '<div class="day-number">'.$list_day.'</div>';
-
-			/** QUERY THE DATABASE FOR AN ENTRY FOR THIS DAY !!  IF MATCHES FOUND, PRINT THEM !! **/
-			$calendar.= str_repeat('<p> </p>',2);
-			
-		$calendar.= '</td>';
-		if ($running_day == 6)
-		{
-			$calendar.= '</tr>';
-			if (($day_counter + 1) != $days_in_month)
-			{
-				$calendar.= '<tr class="calendar-row">';
-			}
-			$running_day = -1;
-			$days_in_week = 0;
-		}
-		$days_in_week++; 
-		$running_day++; 
-		$day_counter++;
-	}
-
-	//Finish the rest of the days in the week
-	if ($days_in_week < 8 && $days_in_week != 1)
-	{
-		for ($x = 1; $x <= (8 - $days_in_week); $x++)
-		{
-			$calendar.= '<td class="calendar-day-blank"> </td>';
-		}
-	}
-
-	//Final row
-	$calendar.= '</tr>';
-
-	//End table
-	$calendar.= '</table>';
 	
-	//Return result
-	return $calendar;
+	echo '<td class="calendar-day-blank"> </td>';
+	
+	$days_in_week++;
 }
 
-echo "<h2>".$month[date('m') - 1]." ".date('Y')."</h2>";
-/*echo date('Y/m/d H:i:s');*/
-echo draw_calendar(date('m'), date('Y'));
+//Continue days of the month
+for ($list_day = 1; $list_day <= $days_in_month; $list_day++)
+{
+	if ($list_day > date('d'))
+	{
+	
+	echo '<td class="calendar-day">';
+	?>
+	<table border="0" cellpadding="0" cellspacing="0">
+	<tr>
+	
+	<td width="120" height="80" background="lock.png" valign="top">
+	
+	<!--<a href="test.html"><?php echo '<div class="day-number">'.$list_day.'</div>';?></a>-->
+	<?php echo '<div class="day-number">'.$list_day.'</div>';?>
+	</td>
+	
+	</tr>
+	</table>
+		
+	<?php 
+	}
+	else
+	{
+	
+	echo '<td class="calendar-day">';
+	?>
+	<table border="0" cellpadding="0" cellspacing="0">
+	<tr>
+	
+	<td width="120" height="80" background="blank.png" valign="top">
+	
+	<a href="test.php?ID=<?php echo $list_day; ?>"><?php echo '<div class="day-number">'.$list_day.'</div>';?></a>
+	</td>
+	
+	</tr>
+	</table>
+		
+	<?php 
+
+	}
+		//Add in number for day
+		//echo str_repeat('<p> </p>',2);
+		
+		echo '</td>';
+	
+	if ($running_day == 6)
+	{
+		echo '</tr>';
+		if (($day_counter + 1) != $days_in_month)
+		{
+			echo '<tr class="calendar-row">';
+			
+		}
+		$running_day = -1;
+		$days_in_week = 0;
+	}
+	$days_in_week++; 
+	$running_day++; 
+	$day_counter++;
+}
+
+//Finish the rest of the days in the week
+if ($days_in_week < 8 && $days_in_week != 1)
+{
+	for ($x = 1; $x <= (8 - $days_in_week); $x++)
+	{
+		echo '<td class="calendar-day-blank"> </td>';
+	}
+}
+
+
+echo '</tr>';
+echo '</table>';
+
 ?>
 </body>
 
